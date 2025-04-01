25f09c9e4aa84de28a9ccef7f1c0e56ad696a7f7
Nata-nk <137524429+Nata-nk@users.noreply.github.com>
2025-04-01 22:43:24 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
 import ContactForm from './components/ContactForm/ContactForm'
 import ContactList from './components/ContactList/ContactList'
 import SearchBox from './components/SearchBox/SearchBox'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [contact, setContact] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]
-  )
+  const [contact, setContact] = useState(() => {
+
+    const savedCount = localStorage.getItem("feedback-state");
+
+    if (savedCount !== null) {
+      return JSON.parse(savedCount);
+    }
+
+    return [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ];
+
+  });
+
 
   const [filtr, setFilter] = useState('')
+
+  useEffect(() => {
+    localStorage.setItem("feedback-state", JSON.stringify(contact));
+  }, [contact]);
 
   const addContact = (newContact) => {
     setContact((prevContact) => {
